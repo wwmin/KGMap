c32c04be0f6ac7be088fe9d6414a916975c8ab31
wwmin <wwei.min@163.com>
2016-01-19 10:40:18 +0800

    添加测量控件开关事件
    测量时关闭点击查询，不测量时打开点击查询

--- js/MainJS.js
+++ js/MainJS.js
@@ -105,6 +105,20 @@
               defaultLengthUnit: Units.METERS
           }, dom.byId("measurementDiv"));
           measurement.startup();
+
+          measurement.areaToggleButton = function () {
+              this.clearResult();
+              this.setTool("area");
+              if ( areaToggled ){
+                  clickHandler =    dojo.connect(map, "onClick", executeIdentifyTask);
+                  areaToggled = false;
+
+              }else{
+                  dojo.disconnect(clickHandler);
+                  areaToggled = true;
+                  distanceToggled = false;
+              }
+          };
           //MeasureWidget End
           // close the info window when esc is pressed
           map.on("key-down", function (e) {
